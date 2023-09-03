336cb4cb62ffb9d70e18ad13cb92e7859eb8cdd6
hyukjin0419 <21900771@handong.ac.kr>
2023-09-03 13:53:36 +0900

    update functions completed

--- src/main/java/com/mycom/word/Main.java
+++ src/main/java/com/mycom/word/Main.java
@@ -1,9 +1,8 @@
 package com.mycom.word;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        // TODO Auto-generated method stub
+        new WordManager().start();
     }
 }
--- src/main/java/com/mycom/word/WordManager.java
+++ src/main/java/com/mycom/word/WordManager.java
@@ -29,11 +29,18 @@ public class WordManager {
     public void start() {
         while(true) {
             int menu = selectMenu();
-            if(menu == 0) break;
-            if(menu == 4) {
-                wordCRUD.addWord();
-            } else if (menu == 1) {
+            if(menu == 0) {
+                System.out.println("프로그램이 종료.");
+                break;
+            }
+            if(menu == 1) {
                 wordCRUD.listAll();
+            } else if (menu == 4) {
+                wordCRUD.addWord();
+            } else if (menu == 5) {
+                wordCRUD.updateItem();
+            } else if (menu == 6) {
+
             }
         }
     }
